19835d43ca337e9244ce05010ac158178ec0518d
易细 <yangtikun@huawei.com>
2022-04-18 08:43:26 +0000

    Signed-off-by: yangtikun<yangtikun@huawei.com> 已报备，恢复到之前

--- launcher/launcher/src/main/cpp/time_weather_view.cpp
+++ launcher/launcher/src/main/cpp/time_weather_view.cpp
@@ -41,9 +41,9 @@ void TimeWeatherView::SetStyle(Style sty)
     viewGroup_->Invalidate();
 }
 
-void TimeWeatherView::SetPosion(int16_t width, int16_t height, int16_t x, int16_t y)
+void TimeWeatherView::SetPosion(int16_t x, int16_t y, int16_t height, int16_t width)
 {
-    viewGroup_->SetPosition(width, height, y, x);
+    viewGroup_->SetPosition(x, y, width, height);
 }
 
 void TimeWeatherView::SetUpView()
