0bb453577f7a016e73f4ab1d0d51e57b0bbb269e
suwenxiang <suwenxiang@huawei.com>
2022-04-19 16:53:12 +0800

    Description: fix font garbled on wearable device IssueNo: https://gitee.com/openharmony/applications_sample_camera/issues/I4ZRTL Feature or Bugfix: Bugfix Binary Source:No Signed-off-by: suwenxiang <suwenxiang@huawei.com>

--- launcher/launcher/src/main/cpp/swipe_view.cpp
+++ launcher/launcher/src/main/cpp/swipe_view.cpp
@@ -130,7 +130,9 @@ void SwipeView::OnSetUpView()
                 if (memcmp(SCREENSAVER_BUNDLE_NAME, pBundleInfos[j].bundleName, strlen(pBundleInfos[j].bundleName)) == 0) {
                     break;
                 }
-
+				if (pBundleInfos[j].abilityInfos->abilityType == SERVICE) {
+                    break;
+                }
                 AppInfo* app = new AppInfo();
                 app->funcclick_ = AppEvent::ClickEvent;
                 app->funclPress_ = AppEvent::LongPressEvent;
